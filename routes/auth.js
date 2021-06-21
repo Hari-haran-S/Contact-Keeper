5b561ab5c6c26a9374328fec078efeb8cd2bb547
Hari-haran-S <59164672+Hari-haran-S@users.noreply.github.com>
2021-06-21 09:08:59 +0530

    backend implementation

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-
-router.get('/',(req, res) => {
-    res.send(`getting a user`);
-})
-router.post('/',(req,res) => {
-    res.send('Log in user')
-})
+router.get('/', (req, res) => {
+  res.send(`getting a user`);
+});
+router.post('/', (req, res) => {
+  res.send('Log in user');
+});
 
 module.exports = router;
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,21 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send(`getting all contact`);
+});
 
-router.get('/',(req, res) => {
-    res.send(`getting all contact`);
-})
+router.post('/', (req, res) => {
+  res.send('add contact');
+});
 
-router.post('/',(req,res) => {
-    res.send('add contact')
-})
+router.put('/:id', (req, res) => {
+  res.send('Update Contact');
+});
 
-router.put('/:id',(req,res) => {
-    res.send('Update Contact')
-})
-
-
-router.delete('/:id',(req,res) => {
-    res.send('delete Contact')
-})
+router.delete('/:id', (req, res) => {
+  res.send('delete Contact');
+});
 module.exports = router;
