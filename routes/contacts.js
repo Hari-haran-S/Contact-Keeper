d4b0030730752207138b0d0cc42ace94f82b1ee6
Hari-haran-S <59164672+Hari-haran-S@users.noreply.github.com>
2021-06-21 07:26:36 +0530

    Getting started

---
+++ routes/contacts.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.get('/',(req, res) => {
+    res.send(`getting all contact`);
+})
+
+router.post('/',(req,res) => {
+    res.send('add contact')
+})
+
+router.put('/:id',(req,res) => {
+    res.send('Update Contact')
+})
+
+
+router.delete('/:id',(req,res) => {
+    res.send('delete Contact')
+})
+module.exports = router;
